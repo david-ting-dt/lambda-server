06e5d9804c1ec4c2b9a48d0305f36061d8a3e23c
davetingbro <davidting3250@gmail.com>
2020-06-15 14:39:20 +1200

    add dotnet publish

--- build_commands/package.sh
+++ build_commands/package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo Build started on `date`
+dotnet publish
 cp -a ./src/HelloWorld/bin/Debug/netcoreapp3.1/publish/. ./
 aws cloudformation package --template-file lambdatemplate.yaml --s3-bucket "david-ting-dn-lambda-deployment-artifacts" --output-template-file outputtemplate.yaml
