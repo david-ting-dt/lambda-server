68fcee9a7b72c83b13589c0583e344957f1ccab1
davetingbro <davidting3250@gmail.com>
2020-06-15 14:32:24 +1200

    update package.sh

--- build_commands/package.sh
+++ build_commands/package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo Build started on `date`
-cp -a ./src/HelloWorld/bin/Debug/netcoreapp3.1/. ./
+cp -a ./src/HelloWorld/bin/Debug/netcoreapp3.1/publish/. ./
 aws cloudformation package --template-file lambdatemplate.yaml --s3-bucket "david-ting-dn-lambda-deployment-artifacts" --output-template-file outputtemplate.yaml
