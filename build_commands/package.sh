2093f84c799450fe378c1ae6fbcd8700ddfd9076
davetingbro <davidting3250@gmail.com>
2020-06-15 14:58:54 +1200

    set publish destination directory

--- build_commands/package.sh
+++ build_commands/package.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo Build started on `date`
-dotnet publish
-cp -a ./src/HelloWorld/bin/Debug/netcoreapp3.1/publish/. ./
+dotnet publish -o ./
 aws cloudformation package --template-file lambdatemplate.yaml --s3-bucket "david-ting-dn-lambda-deployment-artifacts" --output-template-file outputtemplate.yaml
