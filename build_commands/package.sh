0cd25a5168b346841fd1ff5570f8df924f645cc8
davetingbro <davidting3250@gmail.com>
2020-06-15 14:11:57 +1200

    update package.sh

--- build_commands/package.sh
+++ build_commands/package.sh
@@ -2,5 +2,3 @@
 
 echo Build started on `date`
 aws cloudformation package --template-file lambdatemplate.yaml --s3-bucket "david-ting-dn-lambda-deployment-artifacts" --output-template-file outputtemplate.yaml
-cat outputtemplate.yaml
-ls -R
