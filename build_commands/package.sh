1e2064a13b4ff4758111dabe7b7fac41bb092a73
davetingbro <davidting3250@gmail.com>
2020-06-16 15:03:36 +1200

    move dotnet test into separate build command

--- build_commands/package.sh
+++ build_commands/package.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo Build started on `date`
-dotnet test
 dotnet publish -o ./
 aws cloudformation package --template-file lambdatemplate.yaml --s3-bucket "david-ting-dn-lambda-deployment-artifacts" --output-template-file outputtemplate.yaml
