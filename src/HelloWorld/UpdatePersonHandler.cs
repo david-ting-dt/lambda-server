53b62388817e11b407d23632d656556f2acf403c
davetingbro <davidting3250@gmail.com>
2020-06-25 14:12:16 +1200

    rename variables

--- src/HelloWorld/UpdatePersonHandler.cs
+++ src/HelloWorld/UpdatePersonHandler.cs
@@ -6,6 +6,9 @@ using HelloWorld.Interfaces;
 
 namespace HelloWorld
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class UpdatePersonHandler
     {
         private readonly IDataStore _dataStore;
@@ -22,33 +25,33 @@ namespace HelloWorld
 
         public async Task<APIGatewayProxyResponse> UpdatePerson(APIGatewayProxyRequest request)
         {
-            var oldKey = request.PathParameters["name"];
-            var newKey = request.Body;
+            var oldName = request.PathParameters["name"];
+            var newName = request.Body;
             var requestETag = request.Headers != null && request.Headers.ContainsKey("If-Match") ? 
                 request.Headers["If-Match"] : "";
             try
             {
-                var response = await _dataStore.Put(oldKey, newKey, requestETag);
-                return CreateSuccessUpdateResponse(newKey, request.Path, response.ETag);
+                var response = await _dataStore.Put(oldName, newName, requestETag);
+                return CreateSuccessUpdateResponse(newName, request.Path, response.ETag);
             }
             catch (AmazonS3Exception)
             {
-                return CreateFailUpdateResponse(oldKey);
+                return CreateFailUpdateResponse(oldName);
             }
         }
 
-        private static APIGatewayProxyResponse CreateFailUpdateResponse(string oldKey)
+        private static APIGatewayProxyResponse CreateFailUpdateResponse(string oldName)
         {
             return new APIGatewayProxyResponse
             {
                 StatusCode = 404,
-                Body = $"Cannot update {oldKey} - Resource not found"
+                Body = $"Cannot update {oldName} - Resource not found"
             };
         }
 
-        private static APIGatewayProxyResponse CreateSuccessUpdateResponse(string newKey, string path, string etag)
+        private static APIGatewayProxyResponse CreateSuccessUpdateResponse(string newName, string path, string etag)
         {
-            var location = GetNewLocation(newKey, path);
+            var location = GetNewLocation(newName, path);
             return new APIGatewayProxyResponse
             {
                 StatusCode = 301,
@@ -60,11 +63,11 @@ namespace HelloWorld
             };
         }
 
-        private static string GetNewLocation(string newKey, string path)
+        private static string GetNewLocation(string newName, string path)
         {
             var lastSlashIndex = path.LastIndexOf('/');
             var resourcePath = (lastSlashIndex > -1) ? path.Substring(0, lastSlashIndex) : path;
-            return $"{resourcePath}/{newKey}";
+            return $"{resourcePath}/{newName}";
         }
     }
 }
