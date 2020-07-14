9a55d430c05b29d169987cab9e95fe5267c01661
davetingbro <davidting3250@gmail.com>
2020-07-15 08:59:20 +1200

    span command across multiple lines

--- build_commands/build/build.sh
+++ build_commands/build/build.sh
@@ -2,4 +2,6 @@
 
 echo Build started on `date`
 dotnet publish -o ./
-aws cloudformation package --template-file lambdatemplate.yaml --s3-bucket "david-ting-dn-lambda-deployment-artifacts" --output-template-file outputtemplate.yaml
+aws cloudformation package --template-file lambdatemplate.yaml \
+    --s3-bucket "david-ting-dn-lambda-deployment-artifacts" \
+    --output-template-file outputtemplate.yaml
