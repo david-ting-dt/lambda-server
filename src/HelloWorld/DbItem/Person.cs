f40db0b0b5d7f5579434e4617478ccbc3f144409
davetingbro <davidting3250@gmail.com>
2020-07-15 08:59:37 +1200

    add VersionNumber for optimistic locking

--- src/HelloWorld/DbItem/Person.cs
+++ src/HelloWorld/DbItem/Person.cs
@@ -9,5 +9,7 @@ namespace HelloWorld.DbItem
         public string Id { get; set; }
         [DynamoDBProperty("name")]
         public string Name { get; set; }
+        [DynamoDBVersion]
+        public int? VersionNumber { get; set; }
     }
 }
